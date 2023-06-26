faf28772fca1e3c0809ed217698587853062f072
Mario Alejandro Manzano <mmanzano@uadec.edu.mx>
2023-06-26 13:48:49 -0600

    initial project django/cookiecutter generation

---
+++ simple_product_catalog/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from simple_product_catalog.users.models import User
+from simple_product_catalog.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
